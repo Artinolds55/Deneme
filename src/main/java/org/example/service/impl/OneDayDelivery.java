32579ff904e13f138a0fe1a538bc2bdb74c526cc
Necibe <necibeablimit@hotmail.com>
2024-02-15 16:04:25 +0300

    service

---
+++ src/main/java/org/example/service/impl/OneDayDelivery.java
@@ -0,0 +1,6 @@
+package org.example.service.impl;
+
+import org.example.service.DeliveryService;
+
+public class OneDayDelivery implements DeliveryService {
+}
---
+++ src/main/java/org/example/service/impl/OneWeekDelivery.java
@@ -0,0 +1,6 @@
+package org.example.service.impl;
+
+import org.example.service.DeliveryService;
+
+public class OneWeekDelivery implements DeliveryService {
+}
